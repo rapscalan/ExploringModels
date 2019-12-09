6a037e2df8341b610a8004e764965a61b049890f
Zak Hyslin <rapscalan@gmail.com>
2019-12-08 22:50:05 -0800

    routes to api/book

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-require('supertest');
+const request = require('supertest');
 const bookApp = require('../lib/app');
 require('../lib/utils/connect').connect();
 const disconnect = require('../lib/utils/connect').disconnect;
 
 const testBook = {};
 describe('book test routes', () => {
-  expect('test all crud routes', () => {
-    if('can POST a new book', () => {
+  describe('test all crud routes', () => {
+    it('can POST a new book', () => {
       request(bookApp)
-        .post('/api/post')
+        .post('/api/book')
         .send({
           title: 'Cannery Row',
           author: 'John Steinbeck',
@@ -24,38 +24,31 @@ describe('book test routes', () => {
           pages: 312,
           genre: 'fiction'  
         }));
-          testBook._id = res.body._id;
-        })
+        testBook._id = res.body._id;
+        });
     });
   });
   it('can return all the books with GET', () => {
-    request(app)
+    request(bookApp)
       .get('/api/book')
-      .then(res => expect(res.body).toEqual(expect.any(Array)))
+      .then(res => expect(res.body).toEqual(expect.any(Array)));
   });
   it('can return a book by id', () => request(bookApp)
     .get(`/api/book/${testBook._id}`)
     .then(res => expect(res.body).toEqual(expect.any(Object)))
   );
-  it('can update a book with PUT', () => request(app)
-    .put(`/api/put/${testBook._id}`)
+  it('can update a book with PUT', () => request(bookApp)
+    .put(`/api/book/${testBook._id}`)
     .send({
       pages: 350,
     })
     .then(res => expect(res.body._id).toEqual(testBook._id))
   );
-  it('can delete a book by id with DELETE', () => request(app)
-    .del(`/api/delete/${testBook._id}`)
+  it('can delete a book by id with DELETE', () => request(bookApp)
+    .del(`/api/book/${testBook._id}`)
     .then(res => expect(res.body._id).toEqual(testBook._id))
   );
 });
 afterAll(() => {
   disconnect();
 });
-//Get
-
-//Post
-
-//Put
-
-//Delete
